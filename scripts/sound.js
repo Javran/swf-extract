5b86dc53794b69d7ac095e3a6d7e967b2c35fed6
Javran Cheng <Javran.c@gmail.com>
2018-01-31 01:51:07 -0500

    example

--- scripts/sound.js
+++ scripts/sound.js
@@ -3,10 +3,10 @@ const fs = require('fs')
 const _ = require('lodash')
 const { readFromBufferP, extractSounds } = require('../dist')
 
-const rawData = fs.readFileSync('./239e.swf');
+const rawData = fs.readFileSync('./sound_b_bgm_100.swf');
 
 (async () => {
   const swf = await readFromBufferP(rawData)
   const xs = _.compact(await Promise.all(extractSounds(swf.tags)))
-  console.log(xs)
+  xs.map(mp3 => fs.writeFileSync(`sound-${mp3.soundId}.mp3`, mp3.mp3Data))
 })()
